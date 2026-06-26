30d3632c2ae23cf3fe1f4fb6a1514ab2b3d86314
idriss-mechkene <idriss.mechkene@gmail.com>
2026-06-26 17:52:08 +0100

    add  test controller

---
+++ backend/src/main/java/org/example/authproject/controller/TestController.java
@@ -0,0 +1,35 @@
+package org.example.authproject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/test")
+public class TestController {
+
+
+    @GetMapping("/public")
+    public String testEndpoint() {
+        return "Test endpoint is working!";
+    }
+
+    @GetMapping("/logged-in")
+    public String testPrivateEndpoint() {
+        return "Private endpoint is working!";
+    }
+
+    @GetMapping("/admin")
+    public String testAdminEndpoint() {
+        return "Admin endpoint is working!";
+    }
+    @GetMapping("/haspermission-read")
+    public String testHasPermissionReadEndpoint() {
+        return "Has permission read endpoint is working!";
+    }
+
+    @GetMapping("/haspermission-write")
+    public String testHasPermissionWriteEndpoint() {
+        return "Has permission write endpoint is working!";
+    }
+
+
+}
