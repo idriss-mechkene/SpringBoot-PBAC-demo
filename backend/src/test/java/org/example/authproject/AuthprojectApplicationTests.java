4f8409c99425f6ce8987e8829d64e4b692335b91
idriss-mechkene <idriss.mechkene@gmail.com>
2026-06-26 17:22:04 +0100

    add database (docker posgres)

---
+++ backend/src/test/java/org/example/authproject/AuthprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.authproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
