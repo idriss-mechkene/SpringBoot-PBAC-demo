f319ef9432c1e81c8ab5dd1bdf081faab51597f0
idriss-mechkene <idriss.mechkene@gmail.com>
2026-06-27 11:50:39 +0100

    replace autowired  and remove repository innotations

--- backend/src/main/java/org/example/authproject/service/CustomUserDetailsService.java
+++ backend/src/main/java/org/example/authproject/service/CustomUserDetailsService.java
@@ -2,16 +2,17 @@ package org.example.authproject.service;
 
 import org.example.authproject.entity.CustomUserDetails;
 import org.example.authproject.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import jakarta.inject.Inject;
+
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
-    @Autowired
+    @Inject
     private UserRepository userRepository;
 
 
