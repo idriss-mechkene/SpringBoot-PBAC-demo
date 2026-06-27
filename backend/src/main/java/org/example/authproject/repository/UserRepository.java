0b513582b1a9a669986cb768d7dfbe1f442efcfd
idriss-mechkene <idriss.mechkene@gmail.com>
2026-06-27 11:50:39 +0100

    replace autowired  and remove repository innotations

--- backend/src/main/java/org/example/authproject/repository/UserRepository.java
+++ backend/src/main/java/org/example/authproject/repository/UserRepository.java
@@ -1,12 +1,9 @@
 package org.example.authproject.repository;
 
 import java.util.Optional;
-
 import org.example.authproject.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
